44625edad6ce8355685d8cccec6fd7d40f4f5b70
aftership <aftership@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 05:24:04 +0000

    add new couriers, support 231 couriers
    git-svn-id: https://plugins.svn.wordpress.org/aftership-woocommerce-tracking/trunk@1073890 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- aftership.php
+++ aftership.php
@@ -3,7 +3,7 @@
 	Plugin Name: AfterShip - WooCommerce Tracking
 	Plugin URI: http://aftership.com/
 	Description: Add tracking number and carrier name to WooCommerce, display tracking info at order history page, auto import tracking numbers to AfterShip.
-	Version: 1.3.0
+	Version: 1.3.1
 	Author: AfterShip
 	Author URI: http://aftership.com
 
@@ -56,7 +56,7 @@ if (is_woocommerce_active()) {
 
 				$this->api = new AfterShip_API();
 
-				$options = get_option('aftership_option_name');
+                    $options = get_option('aftership_option_name');
 				if ($options) {
 
 					if (isset($options['plugin'])) {
