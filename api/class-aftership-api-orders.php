d0b3d463d9bfb4c8b3d40983299618da06628afd
aftership <aftership@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 07:59:35 +0000

    tagging version 1.4.3
    git-svn-id: https://plugins.svn.wordpress.org/aftership-woocommerce-tracking/tags/1.4.3@1144332 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api/class-aftership-api-orders.php
+++ api/class-aftership-api-orders.php
@@ -119,25 +119,25 @@ class AfterShip_API_Orders extends AfterShip_API_Resource
 			'order_number' => $order->get_order_number(),
 			'created_at' => $this->server->format_datetime($order_post->post_date_gmt),
 			'updated_at' => $this->server->format_datetime($order_post->post_modified_gmt),
-			'completed_at' => $this->server->format_datetime($order->completed_date, true),
-			'status' => $order->status,
-			'currency' => $order->order_currency,
-			'total' => wc_format_decimal($order->get_total(), 2),
-			'subtotal' => wc_format_decimal($this->get_order_subtotal($order), 2),
-			'total_line_items_quantity' => $order->get_item_count(),
-			'total_tax' => wc_format_decimal($order->get_total_tax(), 2),
-			'total_shipping' => wc_format_decimal($order->get_total_shipping(), 2),
-			'cart_tax' => wc_format_decimal($order->get_cart_tax(), 2),
-			'shipping_tax' => wc_format_decimal($order->get_shipping_tax(), 2),
-			'total_discount' => wc_format_decimal($order->get_total_discount(), 2),
-			'cart_discount' => wc_format_decimal($order->get_cart_discount(), 2),
-			'order_discount' => wc_format_decimal($order->get_order_discount(), 2),
-			'shipping_methods' => $order->get_shipping_method(),
-			'payment_details' => array(
-				'method_id' => $order->payment_method,
-				'method_title' => $order->payment_method_title,
-				'paid' => isset($order->paid_date),
-			),
+//			'completed_at' => $this->server->format_datetime($order->completed_date, true),
+//			'status' => $order->status,
+//			'currency' => $order->order_currency,
+//			'total' => wc_format_decimal($order->get_total(), 2),
+//			'subtotal' => wc_format_decimal($this->get_order_subtotal($order), 2),
+//			'total_line_items_quantity' => $order->get_item_count(),
+//			'total_tax' => wc_format_decimal($order->get_total_tax(), 2),
+//			'total_shipping' => wc_format_decimal($order->get_total_shipping(), 2),
+//			'cart_tax' => wc_format_decimal($order->get_cart_tax(), 2),
+//			'shipping_tax' => wc_format_decimal($order->get_shipping_tax(), 2),
+//			'total_discount' => wc_format_decimal($order->get_total_discount(), 2),
+//			'cart_discount' => wc_format_decimal($order->get_cart_discount(), 2),
+//			'order_discount' => wc_format_decimal($order->get_order_discount(), 2),
+//			'shipping_methods' => $order->get_shipping_method(),
+//			'payment_details' => array(
+//				'method_id' => $order->payment_method,
+//				'method_title' => $order->payment_method_title,
+//				'paid' => isset($order->paid_date),
+//			),
 			'billing_address' => array(
 				'first_name' => $order->billing_first_name,
 				'last_name' => $order->billing_last_name,
@@ -162,37 +162,38 @@ class AfterShip_API_Orders extends AfterShip_API_Resource
 				'postcode' => $order->shipping_postcode,
 				'country' => $order->shipping_country,
 			),
-			'note' => $order->customer_note,
-			'customer_ip' => $order->customer_ip_address,
-			'customer_user_agent' => $order->customer_user_agent,
-			'customer_id' => $order->customer_user,
-			'view_order_url' => $order->get_view_order_url(),
+//			'note' => $order->customer_note,
+//			'customer_ip' => $order->customer_ip_address,
+//			'customer_user_agent' => $order->customer_user_agent,
+//			'customer_id' => $order->customer_user,
+//			'view_order_url' => $order->get_view_order_url(),
 			'line_items' => array(),
-			'shipping_lines' => array(),
-			'tax_lines' => array(),
-			'fee_lines' => array(),
-			'coupon_lines' => array(),
+//			'shipping_lines' => array(),
+//			'tax_lines' => array(),
+//			'fee_lines' => array(),
+//			'coupon_lines' => array(),
 		);
 
 		// add line items
 		foreach ($order->get_items() as $item_id => $item) {
 
-			$product = $order->get_product_from_item($item);
+//			$product = $order->get_product_from_item($item);
 
 			$order_data['line_items'][] = array(
 				'id' => $item_id,
-				'subtotal' => wc_format_decimal($order->get_line_subtotal($item), 2),
-				'total' => wc_format_decimal($order->get_line_total($item), 2),
-				'total_tax' => wc_format_decimal($order->get_line_tax($item), 2),
-				'price' => wc_format_decimal($order->get_item_total($item), 2),
+//				'subtotal' => wc_format_decimal($order->get_line_subtotal($item), 2),
+//				'total' => wc_format_decimal($order->get_line_total($item), 2),
+//				'total_tax' => wc_format_decimal($order->get_line_tax($item), 2),
+//				'price' => wc_format_decimal($order->get_item_total($item), 2),
 				'quantity' => (int)$item['qty'],
-				'tax_class' => (!empty($item['tax_class'])) ? $item['tax_class'] : null,
+//				'tax_class' => (!empty($item['tax_class'])) ? $item['tax_class'] : null,
 				'name' => $item['name'],
-				'product_id' => (isset($product->variation_id)) ? $product->variation_id : $product->id,
-				'sku' => is_object($product) ? $product->get_sku() : null,
+//				'product_id' => (isset($product->variation_id)) ? $product->variation_id : $product->id,
+//				'sku' => is_object($product) ? $product->get_sku() : null,
 			);
 		}
 
+		/*
 		// add shipping
 		foreach ($order->get_shipping_methods() as $shipping_item_id => $shipping_item) {
 
@@ -236,17 +237,28 @@ class AfterShip_API_Orders extends AfterShip_API_Resource
 				'amount' => wc_format_decimal($coupon_item['discount_amount'], 2),
 			);
 		}
+		*/
 
 		// aftership add
 		$options = get_option('aftership_option_name');
 		$plugin = $options['plugin'];
 		if ($plugin == 'aftership') {
+
+//            $result = array();
+//            foreach($this->aftership_fields as $field){
+//                $id = $field['id'];
+//                $result[substr($id,10)] = get_post_meta($order->id, '_' . $field['id'], true);
+//            }
+//            $order_data['aftership']['woocommerce']['trackings'][] = $result;
+
 			$order_data['aftership']['woocommerce']['trackings'][] = array(
 				'tracking_provider' => get_post_meta($order->id, '_aftership_tracking_provider', true),
 				'tracking_number' => get_post_meta($order->id, '_aftership_tracking_number', true),
-				'tracking_shipdate' => get_post_meta($order->id, '_aftership_tracking_shipdate', true),
-				'tracking_postal' => get_post_meta($order->id, '_aftership_tracking_postal', true),
-				'tracking_account' => get_post_meta($order->id, '_aftership_tracking_account', true),
+				'tracking_ship_date' => get_post_meta($order->id, '_aftership_tracking_shipdate', true),
+				'tracking_postal_code' => get_post_meta($order->id, '_aftership_tracking_postal', true),
+				'tracking_account_number' => get_post_meta($order->id, '_aftership_tracking_account', true),
+                'tracking_key' => get_post_meta($order->id, '_aftership_tracking_key', true),
+                'tracking_destination_country' => get_post_meta($order->id, '_aftership_tracking_destination_country', true),
 			);
 		} else if ($plugin == 'wc-shipment-tracking') { //$49
 			$order_data['aftership']['woocommerce']['trackings'][] = array(
@@ -435,7 +447,7 @@ class AfterShip_API_Orders extends AfterShip_API_Resource
 
 		$query_args = $this->merge_query_args($query_args, $args);
 
-		return new WP_Query($query_args);
+        return new WP_Query($query_args);
 	}
 
 	/**
